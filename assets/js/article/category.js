d044c13493b89c124a0925346708dc63a104303a
zhaoxiaofei <123@qq.com>
2020-06-12 00:11:47 +0800

    分类的添加、删除

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -21,6 +21,9 @@ function renderCategory() {
 
 // 入口函数
 $(function () {
+    var form = layui.form;
+    var addIndex;
+    var editIndex; // 表示编辑的弹层
     // ------------------ 分类查询、通过模板引擎渲染到页面 ---------------
     renderCategory();
 
@@ -47,6 +50,85 @@ $(function () {
                 }
             });
             layer.close(index);
+        });
+    });
+
+    // ------------------ 点击 添加类别，弹层 ---------------------------
+    $('.layui-card-header button').click(function () {
+        addIndex = layer.open({
+            type: 1,
+            title: '添加类别',
+            content: $('#tpl-add').html(),
+            area: ['400px', '240px']
+        });
+    });
+
+    // ------------------ 点击 确认添加，完成添加功能---------------------
+    // 使用事件委托方式注册submit事件
+    // 给form表单添加一个id
+    $('#tpl-add form').on('submit', '#add-form', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                // 无论成功或失败，提示
+                layer.msg(res.message);
+                if (res.status === 0) {
+                    // 添加成功 重新渲染
+                    renderCategory();
+                    // 关闭弹层
+                    layer.close(addIndex);
+                }
+            }
+        });
+    });
+
+    // ----------------- 点击 编辑，弹层 --------------------------------
+    $('body').on('click', 'edit', function () {
+        // 获取三个自定义属性
+        /*  var id = $(this).attr('data-id');
+         var name = $(this).attr('data-name');
+         var alias = $(this).attr('data-alias'); */
+
+        var data = this.dataset; // dataset 是DOM属性，使用DOM对象this
+        editIndex = layer.open({
+            type: 1,
+            title: '编辑类别',
+            content: $('#tpl-edit').html(),
+            area: ['400px', '240px'],
+            success: function () {
+
+                // edit-form 是表单的lay-filter属性值
+                /* form.val('edit-form', {
+                    id: id,
+                    name: name,
+                    alias: alias
+                }); */
+                form.val('edit-form', JSON.parse(JSON.stringify(data)));
+            }
+        });
+    });
+
+    // ----------------- 点击 确认修改，ajax提交数据完成修改 --------------
+    $('body').on('submit', '#edit-form', function (e) {
+        e.preventDefault();
+        // var data = $(this).serialize();
+        var data = $(this).serializeArray();
+        data[0].name = 'Id';
+
+        $.ajax({
+            type: 'POST',
+            url: '/my/article/updatecate',
+            data: data,
+            success: function (res) {
+                layer.msg(res.message);
+                if (res.status === 0) {
+                    renderCategory();
+                    layer.close(editIndex);
+                }
+            }
         });
     });
 });
