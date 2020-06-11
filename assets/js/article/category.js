1e8b28b51a8d903e474514b63f049bc924688598
zhaoxiaofei <123@qq.com>
2020-06-11 22:55:08 +0800

    删除分类功能已完成

--- assets/js/article/category.js
+++ assets/js/article/category.js
@@ -23,4 +23,30 @@ function renderCategory() {
 $(function () {
     // ------------------ 分类查询、通过模板引擎渲染到页面 ---------------
     renderCategory();
+
+    // ------------------ 删除分类功能 ---------------------------------
+    // 找到删除按钮，注册单击事件，询问是否要删除
+    $('body').on('click', '.delete', function () {
+        // 在询问之前，先获取id
+        var id = $(this).attr('data-id');
+        layer.confirm('确定要删除吗?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            $.ajax({
+                // 把接口中的 :id 换成实际的数字即可（新的传参方式）
+                url: '/my/article/deletecate/' + id,
+                // url: '/my/article/deletecate/1' // 删除id为1的分类
+                success: function (res) {
+                    layer.msg(res.message);
+                    if (res.status === 0) {
+                        // 如果删除成功 则重新渲染
+                        renderCategory();
+                    }
+                }
+            });
+            layer.close(index);
+        });
+    });
 });
